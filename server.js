42933ab96eb51f56a0a89365df2cb24042c3b6f5
Jens <baetenjens07@gmail.com>
2022-03-25 17:10:32 +0100

    Get Rick rolled

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 80;
+
+// middelware
+app.use(express.static("./"));
+app.use(express.json());
+
+app.get('/', (req, res) =>{
+    res.sendFile(__dirname + '/html/index.html')
+})
+
+
+app.listen(PORT, ()=>{
+    console.log(`server running on port ${PORT}`)
+})
